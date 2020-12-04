1e95d7d73f13b0873dd896205cf569df5d47bdd1
César Alves <Cesar.AlvesdeAzevedo-ext@ab-Inbev.com>
2020-12-04 14:56:12 -0300

    first commit

---
+++ TrailerFlix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrailerFlix
+//
+//  Created by César Alves de Azevedo on 04/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
